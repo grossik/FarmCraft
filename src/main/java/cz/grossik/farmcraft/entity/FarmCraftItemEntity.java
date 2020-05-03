7d1573bfeafc64d139c92f0c433be874f7cbf820
grossik <grossikgaming@gmail.com>
2020-05-03 20:53:16 +0200

    Change motion in Item

--- src/main/java/cz/grossik/farmcraft/entity/FarmCraftItemEntity.java
+++ src/main/java/cz/grossik/farmcraft/entity/FarmCraftItemEntity.java
@@ -1,35 +1,42 @@
 package cz.grossik.farmcraft.entity;
 
+import java.util.Objects;
+import java.util.UUID;
+
 import cz.grossik.farmcraft.init.ItemInit;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.MoverType;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.IPacket;
+import net.minecraft.stats.Stats;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
 
 public class FarmCraftItemEntity extends ItemEntity {
-		
 	public FarmCraftItemEntity(EntityType<? extends ItemEntity> p_i50217_1_, World p_i50217_2_) {
 		super(p_i50217_1_, p_i50217_2_);
 	}
 	
-	public FarmCraftItemEntity(EntityType<? extends ItemEntity> p_i50217_1_, World p_i50217_2_, BlockPos pos, ItemStack stack) {
+	public FarmCraftItemEntity(EntityType<? extends ItemEntity> p_i50217_1_, World p_i50217_2_, double posX, double posY, double posZ, ItemStack stack) {
 		super(p_i50217_1_, p_i50217_2_);
-		this.setPosition(pos.getX(), pos.getY(), pos.getZ());
+		this.setPosition(posX, posY, posZ);
 		this.setItem(stack);
 		this.lifespan = (stack.getItem() == null ? 6000 : stack.getEntityLifespan(p_i50217_2_));
-		this.rotationYaw = this.rand.nextFloat() * 360.0F;
-		this.setMotion(this.rand.nextDouble() * 0.2D - 0.1D, 0.2D, this.rand.nextDouble() * 0.2D - 0.1D);
 	}
 	   
 	@Override
 	public IPacket<?> createSpawnPacket() {
 		return NetworkHooks.getEntitySpawningPacket(this);
 	}
-	   
+
 	/*@Override
 	public void tick() {
 		System.err.println("tick");
@@ -42,9 +49,4 @@ public class FarmCraftItemEntity extends ItemEntity {
 		}
 		super.tick();
 	}*/
-	
-	@Override
-	public void onCollideWithPlayer(PlayerEntity entityIn) {
-		
-	}
 }
--- src/main/java/cz/grossik/farmcraft/item/ItemBarley.java
+++ src/main/java/cz/grossik/farmcraft/item/ItemBarley.java
@@ -1,11 +1,15 @@
 package cz.grossik.farmcraft.item;
 
+import java.util.Random;
+
 import cz.grossik.farmcraft.entity.FarmCraftItemEntity;
 import cz.grossik.farmcraft.init.EntityInit;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class ItemBarley extends Item {
@@ -21,12 +25,24 @@ public class ItemBarley extends Item {
 	
 	@Override
 	public Entity createEntity(World world, Entity location, ItemStack itemstack) {
-		return new FarmCraftItemEntity(EntityInit.ITEM_ENTITY.get(), world, location.getPosition(), itemstack);
+			Random rand = new Random();
+	      if (itemstack.isEmpty()) {
+	          return null;
+	       } else {
+	          double d0 = location.getPosYEye() - (double)0.3F;
+	          FarmCraftItemEntity itementity = new FarmCraftItemEntity(EntityInit.ITEM_ENTITY.get(), world, location.getPosX(), d0, location.getPosZ(), itemstack);
+	          itementity.setPickupDelay(40);
+   
+	            float f7 = 0.3F;
+	            float f8 = MathHelper.sin(location.rotationPitch * ((float)Math.PI / 180F));
+	            float f2 = MathHelper.cos(location.rotationPitch * ((float)Math.PI / 180F));
+	            float f3 = MathHelper.sin(location.rotationYaw * ((float)Math.PI / 180F));
+	            float f4 = MathHelper.cos(location.rotationYaw * ((float)Math.PI / 180F));
+	            float f5 = rand.nextFloat() * ((float)Math.PI * 2F);
+	            float f6 = 0.02F * rand.nextFloat();
+	            itementity.setMotion((double)(-f3 * f2 * 0.3F) + Math.cos((double)f5) * (double)f6, (double)(-f8 * 0.3F + 0.1F + (rand.nextFloat() - rand.nextFloat()) * 0.1F), (double)(f4 * f2 * 0.3F) + Math.sin((double)f5) * (double)f6);
+	          
+	          return itementity;
+	       }	      
 	}
-	
-	/*@Override
-	public boolean onEntityItemUpdate(ItemStack stack, ItemEntity entity) {
-		return true;
-	}*/
-	
 }
